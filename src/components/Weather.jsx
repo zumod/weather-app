51e3896944005f009f0aff5bc65c19336af88fe2
Sumod Sivadas <contactsumod@gmail.com>
2020-12-14 11:04:56 +0530

    Added openweathermap API with Axios

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import axios from 'axios';
 
-export default function Weather() {
-    return (
-        <div>
-            
-        </div>
-    )
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'f60b04e12f80a25765bf59524422dcb7';
+
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
 }
